759a0f8558cb1639ba2dbd6a330b929f5da34b3c
icarus2526 <kittitat@dethtechsystems.com>
2018-01-15 19:55:53 +0700

    sss
    ddd

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+//defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
@@ -20,6 +20,6 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('home');
 	}
 }
